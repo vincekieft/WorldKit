b094ff800c29db7431c8b943837829b97ec86843
unknown <vincekieft1997@gmail.com>
2019-05-12 03:00:54 +0200

    Added  rigid and billowy

--- api/procedural/Shaders/Layers/PerlinNoise.hlsl
+++ api/procedural/Shaders/Layers/PerlinNoise.hlsl
@@ -8,7 +8,7 @@ uint PerlinOctaves;
 float PerlinOctavesStrength;
 
 // Methods
-float PerlinNoise(uint id, float amplitude, float2 offset){
+float StandardPerlinNoise(uint id, float amplitude, float2 offset){
     // Calculate vector
     float2 pos = IndexToFloatVector(id, HeightMapResolution);
     
@@ -20,22 +20,33 @@ float PerlinNoise(uint id, float amplitude, float2 offset){
     return cnoise(float2(x * amplitude + offset.x, y * amplitude + offset.y));
 }
 
+float BillowyPerlinNoise(uint id, float amplitude, float2 offset){
+    float noise = StandardPerlinNoise(id, amplitude, offset);
+    return abs(noise * 2 - 1);
+}
+
+float RigidPerlinNoise(uint id, float amplitude, float2 offset){
+    return 1 - BillowyPerlinNoise(id, amplitude, offset);;
+}
+
 // Kernel
 [numthreads(32,1,1)]
 void PerlinNoise (uint3 id : SV_DispatchThreadID)
 {
     if(id.x < HeightMapLength){ // Keep in bounds
-        float baseNoise = PerlinNoise(id.x, PerlinAmplitude, PerlinOffset);
-        float noise = 0; // Perlin noise starts at zero
-        float octaveStrength = 1; // Determines how much of each octave should be added to the final result
+        float total = 0;
+        float frequency = PerlinAmplitude;
+        float amplitude = 1;
+        float maxvalue = 0;
         
-        // Loop through all the octaves
         for(uint i = 0; i < PerlinOctaves; i++){
-            octaveStrength *= 0.5; // Times halve to lower the influence of the octave on the final result
-            noise += PerlinNoise(id.x, pow(abs(PerlinAmplitude), (i + 1)), float2(PerlinOffset.x + (HeightMapResolution * i), PerlinOffset.y + (HeightMapResolution * i))) * octaveStrength;   
+            total += BillowyPerlinNoise(id.x, frequency, float2(0,0)) * amplitude;
+            maxvalue += amplitude;
+            amplitude *= PerlinOctavesStrength;
+            frequency *= 2;
         }
         
         // Update buffer
-        HeightBuffer[id.x] = lerp(baseNoise, noise, PerlinOctavesStrength);
+        HeightBuffer[id.x] = total/maxvalue;
     }
 }
